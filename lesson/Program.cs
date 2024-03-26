443545baebed39767fb6b4a027a711caac1fae94
Voytanovskyy <olegvoyt6@gmail.com>
2024-03-26 20:27:51 +0200

    Add project files.

---
+++ lesson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 1
+            //Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup");
+            //Ex 2
+            int num = 3;
+            int[] arr = new int[num];
+            for
+
+        }
+    }
+}
